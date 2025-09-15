c0c6180eec64539b8cffb419dda4eb0fc7f5fb9e
Mat <ba55dragonofficial999@gmail.com>
2025-09-15 11:18:32 -0600

    Week 2 work done

---
+++ app/week-2/student-info.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function StudentInfo() {
+  return (
+    <div>
+      <h2>Mat Dixon</h2>
+      <p>
+        GitHub Repo:{" "}
+        <a href="https://github.com/n0m4official/cprg306-assignments" target="_blank" rel="noopener noreferrer">
+          https://github.com/n0m4official/cprg306-assignments
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default StudentInfo;
