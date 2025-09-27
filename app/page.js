9919d2da5cbc33da965cb7d11dae254688c869d7
Mat <ba55dragonofficial999@gmail.com>
2025-09-26 18:28:29 -0600

    Week 3 submission - Added week-3 folder - Added Item component for single list items - Added ItemList to render all items - Added Page component with header and list - Tailwind styling with two-color scheme for readability

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ export default function Page() {
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <a href="./week-2">Week 2</a>
+      <a href="./week-3">Week 3</a>
     </main>
   );
 }
